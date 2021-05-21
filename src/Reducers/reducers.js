1d63ecc93cac55744e6010fa4e858ceac29bab3e
Marina Alane <marina.alane@outlook.com>
2021-05-21 17:45:20 -0300

    estrutura

---
+++ src/Reducers/reducers.js
@@ -0,0 +1,6 @@
+// REDUCER PE UMA FUNÇÃO QUE RECEBE O ESTADO ATUAL CORRENTE E RETORNA UM NOVO ESTADO
+import { combineReducers } from 'redux';
+
+const rootReducer = combineReducers({});
+
+export default rootReducer;
