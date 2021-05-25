a9aef4a76b50defeefb6627bb305a23ff9629123
Marina Alane <marina.alane@outlook.com>
2021-05-25 11:16:36 -0300

    primeiro bug da aplicaçao corrigido

--- src/ListTodo.js
+++ src/ListTodo.js
@@ -1,14 +1,20 @@
 import React from 'react';
  
 class ListTodo extends React.Component {
+
+
+clickChange ({event}) {
+  event.setState()
+}
+
   render() {
     return(
       <div>
         <h1>TO DO LIST</h1>
         <input type="text" placeholder="tarefa" />
-        <buton type="button">
+        <button type="button">
           Adiciona tarefa
-        </buton>
+        </button>
       </div>
     )
   }
